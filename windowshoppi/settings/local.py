997143115b088b93e31ff6e24b8fd6c24a536bb2
Ubuntu <ubuntu@ip-172-31-19-89.us-east-2.compute.internal>
2020-09-04 13:19:36 +0000

    updates

--- windowshoppi/settings/local.py
+++ windowshoppi/settings/local.py
@@ -1,9 +1,9 @@
-from .base import *
+irom .base import *
 from .crecidential import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.62', '192.168.43.155']
+ALLOWED_HOSTS = ['3.17.145.13']
 
 DATABASES = {
     'default': {
